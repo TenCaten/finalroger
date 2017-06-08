b5e606111b039eede948693147694e8d347c6827
vinicius <viniciustc41@gmail.com>
2017-06-07 22:01:20 -0300

    alterado novo mundo

--- src/main/java/com/mycompany/aularoger/Main.java
+++ src/main/java/com/mycompany/aularoger/Main.java
@@ -11,6 +11,6 @@ package com.mycompany.aularoger;
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Ola mundo novo teste final!");
+        System.out.println("Ola mundo novo!");
     }
 }
