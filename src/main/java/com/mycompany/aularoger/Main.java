f98e01e92d980e97c14ad4cbb27b5cd83810b82a
vinicius <viniciustc41@gmail.com>
2017-06-07 21:37:15 -0300

    alterado

--- src/main/java/com/mycompany/aularoger/Main.java
+++ src/main/java/com/mycompany/aularoger/Main.java
@@ -11,6 +11,6 @@ package com.mycompany.aularoger;
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Ola mundo!");
+        System.out.println("Ola mundo novo!");
     }
 }
